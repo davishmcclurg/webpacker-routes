f631cf8a3e0fa720a01cb107e2618066d09a55c8
David Harsha <davishmcclurg@gmail.com>
2018-08-30 11:21:47 -0700

    Install webpacker

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,6 +3,7 @@ require_relative 'boot'
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
+require "webpacker"
 require "webpacker/routes"
 
 module Dummy
@@ -16,4 +17,3 @@ module Dummy
     # the framework and any gems in your application.
   end
 end
-
