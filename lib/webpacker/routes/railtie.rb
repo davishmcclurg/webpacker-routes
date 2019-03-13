8c82f30135da5bc22994e643a77ee623a3997022
David Harsha <davishmcclurg@gmail.com>
2019-03-13 08:25:25 -0700

    Support Rails 4 reloader

--- lib/webpacker/routes/railtie.rb
+++ lib/webpacker/routes/railtie.rb
@@ -3,7 +3,11 @@ module Webpacker
     class Engine < ::Rails::Engine
       config.after_initialize do |app|
         generate = -> { Webpacker::Routes.generate(app.tap(&:reload_routes!).routes) }
-        app.reloader.to_run(&generate)
+        if Rails::VERSION::MAJOR >= 5
+          app.reloader.to_run(&generate)
+        else
+          ActionDispatch::Reloader.to_prepare(&generate)
+        end
         generate.call unless ENV['WEBPACKER_ROUTES_INSTALL'] == 'true'
       end
     end
