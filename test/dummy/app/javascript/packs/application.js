6592b4c3b0b8d4d5b82d24cdd25186c6b2eb2c73
David Harsha <davishmcclurg@gmail.com>
2019-03-14 10:02:35 -0700

    Add application pack for testing manually

---
+++ test/dummy/app/javascript/packs/application.js
@@ -0,0 +1,4 @@
+import { root_path, root_url } from 'routes'
+
+console.log(root_path())
+console.log(root_url({ host: 'https://example.com' }))
--- test/webpacker/routes_test.rb
+++ test/webpacker/routes_test.rb
@@ -7,14 +7,14 @@ class Webpacker::Routes::Test < ActiveSupport::TestCase
   delegate :root_path, :source_entry_path, :routes_path, :public_path, :to => :'Webpacker.config'
 
   def compile(javascript, routes: Rails.application.routes, pack: 'test.js')
-    [source_entry_path, routes_path].each do |dir|
-      dir.rmtree if dir.exist?
-      dir.mkdir
-    end
+    pack_file = source_entry_path.join(pack)
+    pack_file.delete if pack_file.exist?
+    pack_file.write(javascript)
     Webpacker::Routes.generate(routes)
-    source_entry_path.join(pack).write(javascript)
     return false unless Dir.chdir(root_path) { Webpacker.compile }
-    public_path.join(Webpacker.manifest.lookup('test.js').slice(1..-1)).read
+    public_path.join(Webpacker.manifest.lookup(pack).slice(1..-1)).read
+  ensure
+    pack_file.delete
   end
 
   test 'generates url/path helpers' do
@@ -50,6 +50,7 @@ class Webpacker::Routes::Test < ActiveSupport::TestCase
   test 'tree-shakes unused routes' do
     with_routing do |set|
       set.draw do
+        root 'application#index'
         get '/used/for/something', as: :used
         get '/tree/shook', as: :tree_shook
       end
